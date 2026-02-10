070ae4819e9443c30a2eb4e105a41c9cd3e790f7
ctnguyen <nguyenbku97@gmail.com>
2026-02-10 15:46:18 +0800

    add autosave

--- scripts/basic_realsense_load_save.py
+++ scripts/basic_realsense_load_save.py
@@ -4,15 +4,17 @@ import argparse
 import natsort
 import numpy as np
 import pyrealsense2 as rs
+import time 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="load realsense, then save", 
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output', type=str, default="", required=True)
+    parser.add_argument('-k', '--keyboard', type=int, default=1, required=False)
+    parser.add_argument('-f', '--fps', type=int, default=5, required=False)
     args = parser.parse_args()
     
     return args
-
 
 if __name__ == "__main__":
     print("Load realsense camera, then save image as required")
@@ -20,6 +22,16 @@ if __name__ == "__main__":
 
     # initialize 
     output_path = args.output
+    save_keyboard = bool(args.keyboard)
+    fps = args.fps
+    #
+    last_t = time.time()
+    SAVE_INTERVAL = 1.0 / fps # seconds
+    if not save_keyboard:
+        print("Use autosave mode")
+    else:
+        print("Use keyboard save mode")
+        
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     img_id = 0
@@ -40,13 +52,22 @@ if __name__ == "__main__":
         cv2.imshow('RealSense Camera', color_image)
 
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('s'):
+        if key == ord('s') and save_keyboard:
             file_name = os.path.join(output_path, str(img_id) + ".png")
             cv2.imwrite(file_name, color_image)
             print(f"Save successfully: {file_name}")
             img_id += 1
-            
-        elif key == ord('q') or key == 27:
+             
+        if not save_keyboard:
+            current_t = time.time()
+            if (current_t - last_t) > SAVE_INTERVAL:
+                file_name = os.path.join(output_path, str(img_id) + ".png")
+                cv2.imwrite(file_name, color_image)
+                print(f"Save successfully: {file_name}")
+                img_id += 1
+                last_t = current_t
+        
+        if key == ord('q') or key == 27:
             print("Close camera")
             break
     
